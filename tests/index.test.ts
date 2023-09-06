b0cab681522114165da3828ae443e8ccda9275f6
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-09-06 11:31:17 +0200

    Move timestamp tests to own file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,15 @@
-const path = require("path");
+import path from "path";
 
+// @ts-ignore
+import Adae from "../../index.node"; // Relative to the destination
 const {
-    Engine,
     Timestamp,
-    inverseMeterScale,
+    Engine,
     meterScale,
+    inverseMeterScale,
     listenForCrash,
     stopListeningForCrash,
-} = require("../../index.node"); // Relative to the destination
+} = Adae;
 
 describe("Engine", () => {
     describe("Constructors", () => {
@@ -386,81 +388,6 @@ describe("Engine", () => {
 
             for (const method of methods) expect(clip[method]).toThrowError();
         });
-    });
-});
-
-describe("Timestamp", () => {
-    test("min()", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(42);
-        const timestamp2 = Timestamp.fromBeatUnits(43);
-        expect(Timestamp.min(timestamp1, timestamp2).getBeatUnits()).toBe(42);
-    });
-    test("max()", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(42);
-        const timestamp2 = Timestamp.fromBeatUnits(43);
-        expect(Timestamp.max(timestamp1, timestamp2).getBeatUnits()).toBe(43);
-    });
-    test("eq()", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(42);
-        const timestamp2 = Timestamp.fromBeatUnits(42);
-        expect(Timestamp.eq(timestamp1, timestamp2)).toBe(true);
-    });
-    test("eq() not equals", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(42);
-        const timestamp2 = Timestamp.fromBeatUnits(43);
-        expect(Timestamp.eq(timestamp1, timestamp2)).toBe(false);
-    });
-
-    test("add()", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(42);
-        const timestamp2 = Timestamp.fromBeatUnits(43);
-        expect(Timestamp.add(timestamp1, timestamp2).getBeatUnits()).toBe(85);
-    });
-    test("sub()", () => {
-        const timestamp1 = Timestamp.fromBeatUnits(43);
-        const timestamp2 = Timestamp.fromBeatUnits(42);
-        expect(Timestamp.sub(timestamp1, timestamp2).getBeatUnits()).toBe(1);
-        expect(() => Timestamp.sub(timestamp2, timestamp1)).toThrowError();
-    });
-    test("mul()", () => {
-        const timestamp = Timestamp.fromBeatUnits(42);
-        expect(Timestamp.mul(timestamp, 2).getBeatUnits()).toBe(84);
-        expect(Timestamp.mul(timestamp, 2.8).getBeatUnits()).toBe(84);
-        expect(() => Timestamp.mul(timestamp, -2)).toThrow();
-    });
-
-    test("zero() is zero", () => {
-        expect(Timestamp.zero().getBeatUnits()).toBe(0);
-    });
-    test("infinity() exists", () => {
-        expect(Timestamp.infinity()).toBeDefined();
-    });
-
-    test("Beat units -> Beat units", () => {
-        const original = 42;
-        const timestamp = Timestamp.fromBeatUnits(original);
-        expect(timestamp.getBeatUnits()).toBe(original);
-    });
-    test("Beats -> Beats", () => {
-        const original = 42;
-        const timestamp = Timestamp.fromBeats(original);
-        expect(timestamp.getBeats()).toBe(original);
-    });
-    test("Samples -> Samples", () => {
-        // The number 375 fits nicely into the roundings of the conversion
-        const original = 375;
-        const timestamp = Timestamp.fromSamples(original, 48_000, 120);
-        expect(timestamp.getSamples(48_000, 120)).toBe(original);
-    });
-    test("Beats -> Beat units", () => {
-        const original = 42;
-        const timestamp = Timestamp.fromBeats(original);
-        expect(timestamp.getBeatUnits()).toBe(original * 1024);
-    });
-    test("Samples -> Beat units", () => {
-        const original = 420;
-        const timestamp = Timestamp.fromSamples(original, 48_000, 120);
-        expect(timestamp.getBeatUnits()).toBe(17);
     });
 });
 
